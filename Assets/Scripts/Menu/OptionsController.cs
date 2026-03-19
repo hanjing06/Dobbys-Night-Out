f4515424e415786ec209474ed77758bfb8224c87
hanjing06 <hanjinglin8@gmail.com>
2026-03-18 20:38:01 -0400

    Add back to main menu title button

---
+++ Assets/Scripts/Menu/OptionsController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsController : MonoBehaviour
+{
+    
+    void BackButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
