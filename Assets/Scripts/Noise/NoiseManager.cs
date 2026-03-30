eba24c8269ad48246c4ed8cfc0e4df2bdb08c749
hkernerm <hadassekernerman@gmail.com>
2026-03-30 09:56:08 -0400

    created scripts to add noises

--- Assets/Scripts/Noise/NoiseManager.cs
+++ Assets/Scripts/Noise/NoiseManager.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NoiseManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static NoiseManager Instance;
+
+    [Header("Noise Settings")]
+    public float currentNoise = 0f;
+    public float maxNoise = 100f;
+
+    [Header("Optional Decay")]
+    public bool noiseDecaysOverTime = false;
+    public float decayRate = 5f;
+
+    private void Awake()
     {
-        
+        // Simple singleton so other scripts can access this manager
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (noiseDecaysOverTime && currentNoise > 0f)
+        {
+            currentNoise -= decayRate * Time.deltaTime;
+            currentNoise = Mathf.Max(currentNoise, 0f);
+        }
+
+        if (currentNoise >= maxNoise)
+        {
+            RestartLevel();
+        }
+    }
+
+    public void AddNoise(float amount)
+    {
+        currentNoise += amount;
+        Debug.Log("Noise: " + currentNoise + " / " + maxNoise);
+    }
+
+    public void RestartLevel()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
     }
 }
--- Assets/Scripts/Noise/ObstacleNoise.cs
+++ Assets/Scripts/Noise/ObstacleNoise.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(AudioSource))]
 public class ObstacleNoise : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Noise Settings")]
+    public float noiseAmount = 20f;
+
+    [Header("Sound")]
+    public AudioClip hitSound;
+
+    [Header("Hit Control")]
+    public float cooldown = 0.5f;
+
+    private AudioSource audioSource;
+    private float lastHitTime = -999f;
+
+    private void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (Time.time < lastHitTime + cooldown)
+            return;
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            lastHitTime = Time.time;
+
+            if (hitSound != null)
+            {
+                audioSource.PlayOneShot(hitSound);
+            }
+
+            if (NoiseManager.Instance != null)
+            {
+                NoiseManager.Instance.AddNoise(noiseAmount);
+            }
+        }
     }
 }
