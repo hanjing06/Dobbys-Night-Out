94cc4b6c76187bebf0f50b4748b33bb356bf63c9
hkernerm <hadassekernerman@gmail.com>
2026-04-01 10:54:38 -0400

    Working on merge conflicts

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -9,7 +9,14 @@ namespace Menu
         public void OnPlayButton()
         {
 
+<<<<<<< HEAD
             SceneManager.LoadScene("Scenes/Level-1/TheClinic/TheClinic"); //need to make it the scene to the actual game
+=======
+            //SceneManager.LoadScene(2); //need to make it the scene to the actual game
+
+            SceneManager.LoadScene("TheClinic"); //need to make it the scene to the actual game
+
+>>>>>>> 160d460 (resolved conflicts)
         }
 
         public void OnQuitButton()
