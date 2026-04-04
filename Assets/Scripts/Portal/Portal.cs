5fdb2c211947982fdefb34707f3520f54eac22bc
hkernerm <hadassekernerman@gmail.com>
2026-04-04 12:15:04 -0400

    Fixed portal issue

--- Assets/Scripts/Portal/Portal.cs
+++ Assets/Scripts/Portal/Portal.cs
@@ -1,19 +1,18 @@
-using System;
 using UnityEngine;
-
 using UnityEngine.SceneManagement;
 
-public class Portal: MonoBehaviour
+public class Portal : MonoBehaviour
 {
     [SerializeField] private string sceneToLoad;
- 
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        Debug.Log("Entered by: " + other.name + " | tag: " + other.tag);
+
+        if (other.CompareTag("Player") || other.transform.root.CompareTag("Player"))
         {
+            Debug.Log("Loading " + sceneToLoad);
             SceneManager.LoadScene(sceneToLoad);
         }
     }
-    
-    
 }
