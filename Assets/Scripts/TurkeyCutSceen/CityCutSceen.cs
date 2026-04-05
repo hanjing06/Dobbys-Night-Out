26cf36685c471d1699a2727deec5b5ac9a3a38ef
OnethWeihenaEpa <oweihena@uwo.ca>
2026-04-05 16:02:20 -0400

    finish level 1

---
+++ Assets/Scripts/TurkeyCutSceen/CityCutSceen.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+
+public class CityCutSceen : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(NextLevel()); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator NextLevel()
+    {
+        yield return new WaitForSeconds(22.32f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
